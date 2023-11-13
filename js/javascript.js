6047d5a9e2d1e681467909a0cc202ac8ef2d97cb
ethansny <143547566+ethansny@users.noreply.github.com>
2023-11-13 00:35:44 -0500

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -2,51 +2,91 @@ var menu = document.getElementById("menu");
 
 var hamburger = document.getElementById("hamburger");
 
-var video = document.getElementById("video_player");
+var hamburger_jcs = document.getElementById("hamburger_jcs");
 
-hamburger.addEventListener("click", function(){
-    console.log("clicked");
-    var scrollPosition = window.scrollY;
-    if(menu.style.top == "0px"){
-        menu.style.top = "-100vh";
-        hamburger.style.transform = "rotate(0deg)";
+var hamburger_projects = document.getElementById("hamburger_projects");
+
+var hamburger_about = document.getElementById("hamburger_about");
+
+var jcsheader = document.querySelector(".title");
+
+var jasonsnyder_name = document.querySelector(".JasonText");
+
+var Ethan_h2 = document.querySelector("#Ethan_h2");
+
+var Ethan_text = document.getElementById("Ethan_text");
+
+// Handles the menue buttons for all pages
+function MenuBtn(hamburgerbtn) {
+    if (menu.style.right == "0px") {
+        menu.style.right = "-100vw";
+        window.scrollTo(0, 0);
+        hamburgerbtn.style.transform = "rotate(0deg)";
         document.body.style.height = "100%";
         document.body.style.overflowY = "auto";
+    } else {
+        menu.style.right = "0";
+        hamburgerbtn.style.transform = "rotate(-90deg)";
+        document.body.style.overflowY = "hidden";
+        hamburgerbtn.style.filter = "invert(0%)";
+    }
+}
+
+
+
+if (hamburger) {
+    hamburger.addEventListener("click", function () {
+        var scrollPosition = window.scrollY;
+        MenuBtn(hamburger);
         if (scrollPosition < 690) {
             hamburger.style.filter = "invert(0%)";
-        } else if(scrollPosition > 691 && scrollPosition < 1200){
+        } else if (scrollPosition > 691 && scrollPosition < 1200) {
             hamburger.style.filter = "invert(100%)";
 
         } else {
             hamburger.style.filter = "invert(0%)";
-    }} else {
-        menu.style.top = "0";
-        hamburger.style.transform = "rotate(-90deg)";
-        document.body.style.overflowY = "hidden";
-        hamburger.style.filter = "invert(0%)";
+        }
+
     }
+    )
+}
+// Hambuger menus for each page
+if (hamburger) {
+    window.addEventListener("scroll", function () {
+        var scrollPosition = window.scrollY;
+        if (scrollPosition < 690) {
+            hamburger.style.filter = "invert(0%)";
+            console.log(scrollPosition);
+        } else if (scrollPosition > 691 && scrollPosition < 1300) {
+            hamburger.style.filter = "invert(100%)";
+            console.log(scrollPosition);
+        } else {
+            hamburger.style.filter = "invert(0%)";
+            console.log(scrollPosition);
+        }
+    })
+}
 
-})
+if (hamburger_jcs) {
+    hamburger_jcs.addEventListener("click", function () {
+        MenuBtn(hamburger_jcs);
+})}
 
+if (hamburger_projects) {
+    hamburger_projects.addEventListener("click", function () {
+        MenuBtn(hamburger_projects);
+})}
 
-window.addEventListener("scroll", function(){
-    var scrollPosition = window.scrollY;
-    if (scrollPosition < 690) {
-        hamburger.style.filter = "invert(0%)";
-        console.log(scrollPosition);
-    } else if(scrollPosition > 691 && scrollPosition < 1300){
-        hamburger.style.filter = "invert(100%)";
-        console.log(scrollPosition);
-    } else {
-        hamburger.style.filter = "invert(0%)";
-        console.log(scrollPosition);
-    }
-})
+if (hamburger_about) {
+    hamburger_about.addEventListener("click", function () {
+        MenuBtn(hamburger_about);
+})}
+
 
 var divs = document.querySelectorAll(".text_boxes div");
 
-divs.forEach(function(div) {
-    div.addEventListener("mousemove", function(e) {
+divs.forEach(function (div) {
+    div.addEventListener("mousemove", function (e) {
         var rect = this.getBoundingClientRect();
         var x = e.clientX - rect.left;
         var y = e.clientY - rect.top;
